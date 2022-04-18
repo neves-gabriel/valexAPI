8df94bae80e674c3d0013482b9fb91dddc9f1b00
neves-gabriel <gabrielrn.mobres@gmail.com>
2022-04-18 12:47:10 -0300

    feat: :sparkles: added recharges features

---
+++ src/routes/rechargesRoutes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import validateApiKey from "../middlewares/validateApiKeyMiddleware";
+import { validateSchema } from "../middlewares/validateSchemaMiddleware";
+import schemas from "../schemas/index";
+import * as controller from "../controllers/rechargesController";
+
+const rechargesRoutes = Router();
+
+rechargesRoutes.post(
+  "/recharges",
+  validateApiKey,
+  validateSchema(schemas.createRechargeSchema),
+  controller.createRecharge
+);
+
+export default rechargesRoutes;
---
+++ src/services/rechargesServices.ts
@@ -0,0 +1,47 @@
+import { Company } from "../repositories/companyRepository";
+import * as cardServices from "./cardsServices";
+import * as employeeServices from "./employeesService";
+import * as rechargeRepository from "../repositories/rechargeRepository";
+import * as errors from "../errors/index";
+import checkAmount from "../utils/checkAmount";
+
+interface Recharge {
+  cardId: number;
+  amount: number;
+}
+
+export async function createRecharge(recharge: Recharge, company: Company) {
+  const { cardId, amount } = recharge;
+
+  const card = await cardServices.getById(cardId);
+
+  const employee = await employeeServices.getById(card.employeeId);
+
+  if (employee.companyId !== company.id) throw errors.Unauthorized();
+
+  cardServices.verifyExpirationDate(card);
+
+  checkAmount(amount);
+
+  const insertRecharge = { cardId, amount };
+
+  await rechargeRepository.insert(insertRecharge);
+}
+
+export async function sumRecharges(cardId: number) {
+  const recharges = await rechargeRepository.findByCardId(cardId);
+
+  const initialValue = 0;
+  const sum = recharges.reduce(
+    (acc, current) => acc + current.amount,
+    initialValue
+  );
+
+  return sum;
+}
+
+export async function getRecharges(cardId: number) {
+  const recharges = await rechargeRepository.findByCardId(cardId);
+
+  return recharges;
+}
