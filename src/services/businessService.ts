ba87f00936f62b140269fca29451dcdd9095e71c
iagod <iago.tostes@cubos.io>
2022-04-27 17:22:11 -0300

    Preparando revisão de código

--- src/services/businessService.ts
+++
@@ -1,8 +0,0 @@
-import * as businessesRepository from "../repositories/businessRepository";
-import * as errors from "../errors/index";
-
-export async function getById(businessId: number) {
-  const business = await businessesRepository.findById(businessId);
-  if (!business) throw errors.NotFound();
-  return business;
-}
